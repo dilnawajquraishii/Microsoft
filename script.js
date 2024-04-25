d6a536c157a9b58918ff1177b06c5eba9de09f2d
dilnawajquraishi <dilnawajquraishi5@gmail.com>
2024-04-25 22:40:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// function border(){
+// // document.getElementById('btn').style.padding="2px";
+// document.getElementById('btn').style.border="2px white solid";
+
+
+// }
+function a(){
+    document.getElementById('dwn').style.display='block';
+
+}
+function b(){
+    document.getElementById('dwn').style.display='none';
+}
+
